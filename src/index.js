66cc12d2296f3fad95fd36f5ad44044be8a54ccb
KevinStifel <80784057+KevinStifel@users.noreply.github.com>
2024-09-30 21:31:57 -0300

    push inicial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import dotenv from 'dotenv';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App.js';
+import reportWebVitals from './reportWebVitals.js';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+// Configurar dotenv para cargar las variables de entorno
+dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain={process.env.DOMAIN}
+      clientId={process.env.CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
 
